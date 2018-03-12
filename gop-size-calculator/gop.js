7560874102fb049a132482c4c8a95989c6c1db09
Anton Lindström <carlantonlindstrom@gmail.com>
2018-03-12 22:37:25 +0100

    update gop-size-calculator

--- gop-size-calculator/gop.js
+++ gop-size-calculator/gop.js
@@ -1,4 +1,5 @@
-var gcd = function (n, m) {
+
+var gcd = function(n, m) {
   var r = 0;
   while (n !== 0) {
     r = m % n;
@@ -7,18 +8,22 @@ var gcd = function (n, m) {
   }
   return m;
 };
-    
-function changeEventHandler(event) {
+
+
+var changeEventHandler = function(event) {
   var frameRate = parseInt(document.querySelector('input[name="frameRate"]').value) || 0;
   var sampleRate = parseInt(document.querySelector('select[name="sampleRate"]').value) || 0;
-  var samplesPerFrame = parseInt(document.querySelector('select[name="samplesPerFrame"]').value) || 0;
-  var gopSize = frameRate * samplesPerFrame / gcd(frameRate * samplesPerFrame, sampleRate);
+  var framesPerPacket = parseInt(document.querySelector('select[name="framesPerPacket"]').value) || 0;
+ 
+  var gopSize = frameRate * framesPerPacket / gcd(frameRate * framesPerPacket, sampleRate);
+ 
+
   var tbody = document.getElementById('tbody');
 
   while (tbody.firstChild) {
     tbody.removeChild(tbody.firstChild);
   }
-  
+
   for (var size = gopSize; size <= 1000; size += gopSize) {
       var row = tbody.insertRow();
       row.insertCell(0).appendChild(document.createTextNode(size));
@@ -28,7 +33,7 @@ function changeEventHandler(event) {
 
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('select[name="sampleRate"]').onchange = changeEventHandler;
-  document.querySelector('select[name="samplesPerFrame"]').onchange = changeEventHandler;
+  document.querySelector('select[name="framesPerPacket"]').onchange = changeEventHandler;
   document.querySelector('input[name="frameRate"]').onchange = changeEventHandler;
 
   changeEventHandler();
