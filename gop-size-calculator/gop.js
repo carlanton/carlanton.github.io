5a4fede4fdeddf871267f540cec950981d97e202
Anton Lindström <carlantonlindstrom@gmail.com>
2018-05-05 16:10:40 +0200

    gop.js: Floating point frame rates are not suppoerted

--- gop-size-calculator/gop.js
+++ gop-size-calculator/gop.js
@@ -1,3 +1,6 @@
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === "number" && isFinite(value) && Math.floor(value) === value;
+};
 
 var gcd = function(n, m) {
   var r = 0;
@@ -9,20 +12,22 @@ var gcd = function(n, m) {
   return m;
 };
 
-
 var changeEventHandler = function(event) {
-  var frameRate = parseInt(document.querySelector('input[name="frameRate"]').value) || 0;
+  var frameRate = parseFloat(document.querySelector('input[name="frameRate"]').value) || 0;
   var sampleRate = parseInt(document.querySelector('select[name="sampleRate"]').value) || 0;
   var framesPerPacket = parseInt(document.querySelector('select[name="framesPerPacket"]').value) || 0;
- 
-  var gopSize = frameRate * framesPerPacket / gcd(frameRate * framesPerPacket, sampleRate);
- 
 
   var tbody = document.getElementById('tbody');
-
   while (tbody.firstChild) {
     tbody.removeChild(tbody.firstChild);
   }
+
+  if (!Number.isInteger(frameRate) || frameRate <= 0) {
+    alert("Only integer frame rates are supported");
+    return;
+  }
+
+  var gopSize = frameRate * framesPerPacket / gcd(frameRate * framesPerPacket, sampleRate);
 
   for (var size = gopSize; size <= 1000; size += gopSize) {
       var row = tbody.insertRow();
